410d59fe9b5c09d29ae63b22cc3d135ea15fa4f0
Travis CI User <travis@example.org>
2018-09-04 21:29:09 +0000

    Documentation for frankmcsherry/dataflow-join@a882fe7

---
+++ implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["bytes"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"bytes/rc/struct.Bytes.html\" title=\"struct bytes::rc::Bytes\">Bytes</a>",synthetic:false,types:["bytes::rc::Bytes"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"bytes/arc/struct.Bytes.html\" title=\"struct bytes::arc::Bytes\">Bytes</a>",synthetic:false,types:["bytes::arc::Bytes"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
